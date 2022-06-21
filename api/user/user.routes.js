f627d9d6a12777980701b61d46c81b4f7794c3b3
SalwaBaqer <s.baqer@hotmail.com>
2022-06-21 19:20:10 +0300

    Add signup routes and controllers

--- api/user/user.routes.js
+++ api/user/user.routes.js
@@ -1,2 +1,10 @@
 const express = require("express");
+const { signup } = require("./user.controllers");
+
+//create a mini express app
 const router = express.Router();
+
+//routes
+router.post("/signup", signup);
+
+module.exports = router;
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  JWT_SECRET: "rawan-and-salwa-are-the-best",
+  JWT_EXPIRATION_MS: 90000000,
+};
